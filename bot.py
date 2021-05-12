6f94d263cb76c747cbd1ada3e2f34e606fb8fae7
ssakk <72147775+ssakk@users.noreply.github.com>
2021-05-12 21:05:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,55 @@
 import telebot
 from telebot import types
+import random                               # импорты
 
-bot = telebot.TeleBot('1760095413:AAEGj4-Zl_Ux28_10xXphDH1Ni5t0LsfkEk')
+bot = telebot.TeleBot('1842261563:AAFUP-lfLPTRhEkSfPoNM9hFLB53ltxbwLQ')             # токен
+
+MEMORY = dict()                 # хранитель данных
 
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "/start":
-        bot.send_message(message.from_user.id, "Привет! Этот бот-апвоут, в котором вы можете прочитать интересные факты"
-                                               " и истории людей, а взамен перепечатать увиденную историю и помочь нам "
-                                               "с проектом! При печати не требуется отключать Т9, нужно лишь не "
-                                               "исправлять никакие ошибки в тексте))")
-        keyboard = types.InlineKeyboardMarkup()
-        key_1 = types.InlineKeyboardButton(text='Какой фильм законился бы через 10 минут, если бы герой не тупил?',
-                                           callback_data='number1')
-        keyboard.add(key_1)
-        key_2 = types.InlineKeyboardButton(text='Как думаете, что нас ждёт после смерти?',
-                                           callback_data='number2')
-        keyboard.add(key_2)
-        key_3 = types.InlineKeyboardButton(text='Крутые факты про динозавров',
-                                           callback_data='number3')
-        keyboard.add(key_3)
-        bot.send_message(message.from_user.id, text='Выбери тему!', reply_markup=keyboard)
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши /start и бот начнет работу. Напиши /menu и откроется меню выбора"
-                                               " темы.")
-    elif message.text == "/menu":
-        keyboard = types.InlineKeyboardMarkup()
-        key_1 = types.InlineKeyboardButton(text='Какой фильм законился бы через 10 минут, если бы герой не тупил?',
-                                           callback_data='number1')
-        keyboard.add(key_1)
-        key_2 = types.InlineKeyboardButton(text='Как думаете, что нас ждёт после смерти?',
-                                           callback_data='number2')
-        keyboard.add(key_2)
-        key_3 = types.InlineKeyboardButton(text='Крутые факты про динозавров',
-                                           callback_data='number3')
-        keyboard.add(key_3)
-        bot.send_message(message.from_user.id, text='Выбери тему!', reply_markup=keyboard)
-    else:
-        bot.send_message(message.from_user.id, 'Я тебя не понимаю(( Напиши /help')
+def send_memorize(user_id, text, keyboard=None):
+    MEMORY[user_id] = {"text": text, "is upvote": False}
+    bot.send_message(user_id, text, reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['start'])                    # команда /start
+def send_welcome(message):
+    bot.send_message(message.from_user.id, "Привет! Этот бот-апвоут, в котором вы можете прочитать интересные факты"
+                                           " и истории людей, а взамен перепечатать увиденную историю и помочь нам "
+                                           "с проектом! При печати не требуется отключать Т9, нужно лишь не "
+                                           "исправлять никакие ошибки в тексте))")
+    keyboard = types.InlineKeyboardMarkup()
+    key_1 = types.InlineKeyboardButton(text='Какой фильм законился бы через 10 минут, если бы герой не тупил?',
+                                       callback_data='number1')
+    keyboard.add(key_1)
+    key_2 = types.InlineKeyboardButton(text='Как думаете, что нас ждёт после смерти?',
+                                       callback_data='number2')
+    keyboard.add(key_2)
+    key_3 = types.InlineKeyboardButton(text='Крутые факты про динозавров',
+                                       callback_data='number3')
+    keyboard.add(key_3)
+    bot.send_message(message.from_user.id, text='Выбери тему!', reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['help'])                # команда /help
+def send_help(message):
+    bot.send_message(message.from_user.id, "Напиши /start и бот начнет работу. Напиши /menu и откроется меню выбора"
+                                           " темы.")
+
+
+@bot.message_handler(commands=['menu'])                 # команда /menu
+def send_menu(message):
+    keyboard = types.InlineKeyboardMarkup()
+    key_1 = types.InlineKeyboardButton(text='Какой фильм законился бы через 10 минут, если бы герой не тупил?',
+                                       callback_data='number1')
+    keyboard.add(key_1)
+    key_2 = types.InlineKeyboardButton(text='Как думаете, что нас ждёт после смерти?',
+                                       callback_data='number2')
+    keyboard.add(key_2)
+    key_3 = types.InlineKeyboardButton(text='Крутые факты про динозавров',
+                                       callback_data='number3')
+    keyboard.add(key_3)
+    bot.send_message(message.from_user.id, text='Выбери тему!', reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -47,26 +58,20 @@ def callback_worker(call):
         with open('1.txt', encoding='utf-8') as f:
             text = f.read()
         splited_text = text.split('$')
-        for i in splited_text:
-            bot.send_message(call.message.chat.id, i)
-            keyboard = types.InlineKeyboardMarkup()
-            key_next = types.InlineKeyboardButton(text='Следующий апвоут', callback_data='next')
-            keyboard.add(key_next)
-            key_previous = types.InlineKeyboardButton(text='Предыдущий апвоут', callback_data='previous')
-            keyboard.add(key_previous)
-            bot.send_message(call.from_user.id, text='Выбери, что делать дальше', reply_markup=keyboard)
+        i = random.randint(0, len(splited_text))
+        bot.send_message(call.message.chat.id, splited_text[i])
     if call.data == "number2":
         with open('2.txt', encoding='utf-8') as g:
             text = g.read()
         splited_text = text.split('$')
-        for k in splited_text:
-            bot.send_message(call.message.chat.id, k)
+        k = random.randint(0, len(splited_text))
+        bot.send_message(call.message.chat.id, splited_text[k])
     if call.data == "number3":
         with open('3.txt', encoding='utf-8') as t:
             text = t.read()
         splited_text = text.split('$')
-        for j in splited_text:
-            bot.send_message(call.message.chat.id, j)
+        j = random.randint(0, len(splited_text))
+        bot.send_message(call.message.chat.id, splited_text[j])
 
 
 bot.polling(none_stop=True, interval=0)
